97ca6fb4a3d9f5274275a55c959d4bf841ea80ad
shuvomondalofficial70 <shuvomondalofficial70@gmail.com>
2026-01-07 15:01:43 +0530

    first commit

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+
+var typed = new Typed("#typed-text", {
+    strings: ["Web Developer", "Freelancer", "Web Designer"],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true
+});
+
